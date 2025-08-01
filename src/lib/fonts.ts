48f31318e8aa06680ff8774835fc5ad6d0bfdfa5
AlexandruJungean <alex.jungean@gmail.com>
2025-08-02 00:02:15 +0300

    Fonts, Favicon, logos

---
+++ src/lib/fonts.ts
@@ -0,0 +1,197 @@
+import localFont from 'next/font/local'
+
+// Poppins font family
+export const poppins = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins/Poppins-Thin.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-ThinItalic.ttf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-ExtraLightItalic.ttf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-LightItalic.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-SemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-ExtraBoldItalic.ttf',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Poppins/Poppins-BlackItalic.ttf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-poppins',
+  display: 'swap',
+})
+
+// Sonny Gothic font family
+export const sonnyGothic = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-Thin.otf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-ThinItalic.otf',
+      weight: '100',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-UltraLight.otf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-UltraLightItalic.otf',
+      weight: '200',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-Light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-LightItalic.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-RegularItalic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-Book.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-BookItalic.otf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-BoldItalic.otf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-ExtraBold.otf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-ExtraBoldItalic.otf',
+      weight: '800',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-Black.otf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-BlackItalic.otf',
+      weight: '900',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-UltraBlack.otf',
+      weight: '950',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Sonny Gothic/SonnyGothic-UltraBlackItalic.otf',
+      weight: '950',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-sonny-gothic',
+  display: 'swap',
+}) 
